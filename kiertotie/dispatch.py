c011865819e6607542d84f4113ba1c006b3f6560
Stefan Hagen <stefan@hagen.link>
2022-11-19 22:40:48 +0100

    Added blunt i18n experimental SMVP

--- kiertotie/dispatch.py
+++ kiertotie/dispatch.py
@@ -1,8 +1,14 @@
 import argparse
+import gettext
+
+de = gettext.translation('base', localedir='locale', languages=['de'])
+de.install()
+_ = de.gettext  # German
 
 
 def verify(options: argparse.Namespace) -> int:
     """Later alligator."""
     if options:
+        print(_('YES'))
         return 0
     return 2
